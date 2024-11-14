71faca05f760531cd7e72abb367847f631b28c85
Eclipse-5214 <elijahlipscomb55@gmail.com>
2024-11-14 08:19:14 -0500

    progress

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+const route = useRoute()
+</script>
+
+<template>
+  <div id="wrapper">
+    <div class="navbar">
+      <NavBar />
+    </div>
+    <div class="content">
+      <UContainer>
+        <h1 id="title">FROST</h1>
+      </UContainer>
+    </div>  
+  </div>
+</template>
